b1f31a3ee630358a8e20e706d703ee925241120e
buddhi raj <rajbuddhi496@gmail.com>
2023-08-19 15:38:08 +0545

    login and registration success

--- app/src/main/java/com/buddhiraj/suitcase/LoginActivity.java
+++ app/src/main/java/com/buddhiraj/suitcase/LoginActivity.java
@@ -1,13 +1,31 @@
 package com.buddhiraj.suitcase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class LoginActivity extends AppCompatActivity {
+
+
+    EditText emailTextView, passwordTextView, forgotPasswordTextView;
+    Button loginButton;
+    ProgressBar divider;
+    FirebaseAuth mAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,5 +43,80 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+        emailTextView = findViewById(R.id.email);
+        passwordTextView = findViewById(R.id.password);
+        forgotPasswordTextView = findViewById(R.id.forgotPassword);
+        loginButton = findViewById(R.id.loginButton);
+        divider = findViewById(R.id.divider);
+
+        // Set on Click Listener on Registration button
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginUserAccount();
+            }
+        });
+
     }
-}
+        private void loginUserAccount()
+        {
+
+            // Take the value of two edit texts in Strings
+            String email, password;
+            email = emailTextView.getText().toString();
+            password = passwordTextView.getText().toString();
+
+            // validations for input email and password
+            if (TextUtils.isEmpty(email)) {
+                Toast.makeText(getApplicationContext(),
+                                "Please enter email!!",
+                                Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            if (TextUtils.isEmpty(password)) {
+                Toast.makeText(getApplicationContext(),
+                                "Please enter password!!",
+                                Toast.LENGTH_LONG)
+                        .show();
+                return;
+            }
+
+            // signin existing user
+            mAuth.signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(
+                            new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(
+                                        @NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        Toast.makeText(getApplicationContext(),
+                                                        "Login successful!!",
+                                                        Toast.LENGTH_LONG)
+                                                .show();
+
+
+                                        // if sign-in is successful
+                                        // intent to home activity
+                                        Intent intent
+                                                = new Intent(LoginActivity.this,
+                                                MainActivity.class);
+                                        startActivity(intent);
+                                    } else {
+
+                                        // sign-in failed
+                                        Toast.makeText(getApplicationContext(),
+                                                        "Email or Password doesn't match!!",
+                                                        Toast.LENGTH_LONG)
+                                                .show();
+
+                                    }
+                                }
+                            });
+        }
+    }
--- app/src/main/java/com/buddhiraj/suitcase/RegistrationActivity.java
+++ app/src/main/java/com/buddhiraj/suitcase/RegistrationActivity.java
@@ -1,14 +1,30 @@
 package com.buddhiraj.suitcase;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.AuthResult;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class RegistrationActivity extends AppCompatActivity {
+
+    EditText userNameTextView, userEmailTextView, userPasswordTextView, conformPasswordTextView;
+    Button signUpButton;
+    ProgressBar progressbar;
+    FirebaseAuth mAuth;
 
 
     @Override
@@ -27,5 +43,83 @@ public class RegistrationActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        // taking FirebaseAuth instance
+        mAuth = FirebaseAuth.getInstance();
+
+        // initialising all views through id defined above
+        userNameTextView = findViewById(R.id.userName);
+        userEmailTextView = findViewById(R.id.userEmail);
+        userPasswordTextView = findViewById(R.id.userPassword);
+        conformPasswordTextView = findViewById(R.id.conformPassword);
+        signUpButton = findViewById(R.id.signUpButton);
+        progressbar = findViewById(R.id.progressbar);
+
+        // Set on Click Listener on Registration button
+        signUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                registerNewUser();
+            }
+        });
+    }
+
+    private void registerNewUser() {
+
+        // show the visibility of progress bar to show loading
+        progressbar.setVisibility(View.VISIBLE);
+
+        // Take the value of two edit texts in Strings
+        String email, password;
+        email = userEmailTextView.getText().toString();
+        password = userPasswordTextView.getText().toString();
+
+        // Validations for input email and password
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter email!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(),
+                            "Please enter password!!",
+                            Toast.LENGTH_LONG)
+                    .show();
+            return;
+        }
+        // create new user or register new user
+        mAuth
+                .createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Toast.makeText(getApplicationContext(),
+                                            "Registration successful!",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+
+                            // if the user created intent to login activity
+                            Intent intent
+                                    = new Intent(RegistrationActivity.this,
+                                    LoginActivity.class);
+                            startActivity(intent);
+                        } else {
+
+                            // Registration failed
+                            Toast.makeText(
+                                            getApplicationContext(),
+                                            "Registration failed!!"
+                                                    + " Please try again later",
+                                            Toast.LENGTH_LONG)
+                                    .show();
+
+                        }
+                    }
+                });
     }
 }
